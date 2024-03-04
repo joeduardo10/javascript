2dbbc30e63bbd3305d8b4b0846f3e85246b8595e
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2024-03-04 20:18:26 -0300

    update

--- ex015/script.js
+++ ex015/script.js
@@ -1,1 +1,11 @@
-alert('ola')
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear
+    var f_ano = document.getElementById('txtano')
+    var res = document.querySelector('div#res') 
+    if(f_ano.value.length == 0 || f_ano > ano){
+        window.alert('Verifique os dados e tente novamente!')
+    } else{
+        window.alert('tudo ok!')
+    }
+}
