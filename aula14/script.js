462206d7075ebdbf986363d087382bfe723eff25
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2024-03-03 22:37:33 -0300

    up

--- aula14/script.js
+++ aula14/script.js
@@ -1,1 +1,18 @@
-alert('ola')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img =window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} Horas`
+    if(hora >= 0 && hora < 12){
+        img.src = "imagens/manha1.png" /*Bom dia*/
+        window.document.body.style.background = '#e2cd9f'
+    } else if(hora >= 12 && hora < 18){
+        img.src = "imagens/tarde.png" /*Boa tarde */
+        document.body.style.background = '#b9846f'
+    }else{
+        img.src = "imagens/noite.png" /*Boa Noite */
+        document.body.style.background = '#515154'
+    }
+    
+}
